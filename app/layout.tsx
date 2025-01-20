a714570b1f8513d8da031239ab4c69471595e6aa
Yanis Mahdi <yanis.mahdi@etu.u-bordeaux.fr>
2025-01-20 17:57:44 +0100

    import fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { Anek_Telugu } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 
