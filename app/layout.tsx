05e0a9dbdb2e0c8605145c0415022515538a690f
Yanis Mahdi <yanis.mahdi@etu.u-bordeaux.fr>
2025-01-24 08:13:38 +0100

    vercel deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { Analytics } from '@vercel/analytics/next';
 
 
 // Importation des polices
@@ -43,6 +44,7 @@ export default function RootLayout({
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
