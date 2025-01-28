4dc470c8a2e15b7cc87b6e2caac90c8317d8060b
Yanis Mahdi <yanis.mahdi@etu.u-bordeaux.fr>
2025-01-28 11:15:23 +0100

    speed_insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Analytics } from '@vercel/analytics/next';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 
 // Importation des polices
