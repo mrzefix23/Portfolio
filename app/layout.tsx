599eb573a00147529e1a6cb2729fe99f145363bb
Yanis Mahdi <yanis.mahdi@etu.u-bordeaux.fr>
2025-01-28 11:11:59 +0100

    Speed_Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 // Importation des polices
@@ -45,6 +46,7 @@ export default function RootLayout({
       >
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
